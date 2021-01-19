bab577c6e2ac21612d5ce23a565f89d31b9db3e6
Anna Blanton <blanton@uw.edu>
2021-01-18 22:16:59 -0800

    Added Wheelie animations

---
+++ sceneManager.js
@@ -0,0 +1,28 @@
+class SceneManager {
+    constructor(game) {
+        this.game = game;
+        this.game.camera = this;
+        this.x = 0;
+
+        game.addEntity(new Wheelie(game, 16, 16));
+    };
+
+    clearEntities() {
+        this.game.entities = [this];
+    };
+
+    loadLevelOne() {
+        
+
+    };
+
+    update() {
+        //PARAMS.DEBUG = document.getElementById("debug").checked;
+    };
+
+    draw(ctx) {
+
+        if (PARAMS.DEBUG) {
+        }
+    };
+};
