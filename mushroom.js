59206e0e5a47c73ffa18f706b46e75826998b415
Caleb Chang <caleb830122@gmail.com>
2021-02-14 01:28:24 -0800

    Added powerup box class in mushroom.js
    New power up item, which currently makes the pallets bigger when Megaman eats the powerup. Still need to fix the animation.

--- mushroom.js
+++ mushroom.js
@@ -56,3 +56,62 @@ class Mushroom {
         }
     }
 }
+
+class Powerup {
+    constructor(game, x, y, type) {
+        Object.assign(this, { game, x, y, type});
+        this.SPRITE_WIDTH = 32;
+        this.SPRITE_HEIGHT = 32;
+        
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/powerup.png");
+        this.emerging = true; //From a block when block being shot? 
+        this.velocity = { x: 0, y: 0};
+        this.updateBB();
+        this.effectiveTime = 10000;
+        this.isEffective = false;
+    }
+    updateBB() {
+        this.BB = new BoundingBox(this.x, this.y, 32, 32);
+    }
+    update() {
+        const FALL_ACCELERATION = 1000;
+
+        this.velocity.y += FALL_ACCELERATION * this.game.clockTick;
+        this.x += this.game.clockTick * this.velocity.x * PARAMS.SCALE;
+        this.y += this.game.clockTick * this.velocity.y * PARAMS.SCALE;
+        this.updateBB();
+        
+        var that = this;
+        this.game.entities.forEach(function (entity) {
+            if (entity.BB && that.BB.collide(entity.BB)) {
+                if (entity instanceof Megaman) {
+                    that.removeFromWorld = true;
+                    entity.weaponLevel = 1;
+                } else if ((entity instanceof Tile)
+                    && (that.BB.bottom - that.velocity.y * that.game.clockTick * PARAMS.SCALE) <= entity.BB.top) {
+                    that.y = entity.BB.top - that.SPRITE_HEIGHT;
+                    that.velocity.y = 0;
+                    that.updateBB();
+                } else if (entity !== that) {
+                    that.velocity.x = -that.velocity.x;
+                }
+            };
+        });
+        
+        if (this.isEffective == true) {
+            this.effectiveTime -= this.game.clockTick;
+        }
+        if (this.effectiveTime == 0) {
+            this.shrinkPelletSize();
+        }
+    }
+    
+    draw(ctx) {
+
+        ctx.drawImage(this.spritesheet, 0, 0, 336, 336, this.x - this.game.camera.x, this.y - this.game.camera.y, this.SPRITE_WIDTH, this.SPRITE_HEIGHT);
+        
+        if (PARAMS.DEBUG) {
+            ctx.strokeRect(this.BB.x - this.game.camera.x, this.BB.y- this.game.camera.y, this.BB.width, this.BB.height);
+        }
+    }
+}
