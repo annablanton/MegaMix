8bfc675e5aa647ee2ecd3515d5471f7b27e43c91
Anna Blanton <blanton@uw.edu>
2021-02-24 23:02:35 -0800

    Created Barba boss battle

--- bosstrigger.js
+++ bosstrigger.js
@@ -1,16 +1,38 @@
 class BossTrigger {
-    constructor(game, x, y, width, height) {
-        Object.assign(game, x, y, width, height);
+    constructor(game, x, y, width, height, boss, walls) {
+        Object.assign(this, { game, x, y, width, height, boss, walls});
+        this.bossFightStarted = false;
+        this.BB = new BoundingBox(this.x, this.y, this.width, this.height);
+        this.tiles = [];
     }
 
     update() {
+        if (!this.bossFightStarted) {
+            if (this.BB.collide(this.game.camera.megaman.BB)) {
+                var that = this;
+                this.walls.forEach(function (wall) {
+                    for (var i = 0; i < 4; i++) {
+                        var tile = new Tile(that.game, wall.x, wall.y + i * 32, 24, 3);
+                        that.tiles.push(tile);
+                        that.game.addEntity(tile);
+                    }
+                });
+                this.bossFightStarted = true;
+            }
+        }
 
+        if (this.boss.removeFromWorld) {
+            this.tiles.forEach(function (tile) {
+                tile.removeFromWorld = true;
+            });
+            this.removeFromWorld = true;
+        }
     }
 
     draw(ctx) {
         if (PARAMS.DEBUG) {
             ctx.strokeStyle = "Blue";
-            ctx.strokeRect(this.x, this.y, this.width, this.height);
+            ctx.strokeRect(this.x - this.game.camera.x, this.y - this.game.camera.y, this.width, this.height);
         }
     }
 }
